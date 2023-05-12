2ffe84b68883e4e0c3198289d16acb7982923bfe
Kanguk Lee <p51lee@kaist.ac.kr>
2023-05-12 15:10:21 +0900

    Re-deinfe matrix and vectors

--- src/Matrix.v
+++ src/Matrix.v
@@ -10,124 +10,119 @@ Bind Scope C_scope with C.
 
 (* Matrix record ================================================================================ *)
 
-Record Matrix_inner: Type := {
-  rows: nat;
-  cols: nat;
-  inner: nat -> nat -> C;
-}.
-
 Record Matrix: Type := {
-  inner_mat: Matrix_inner;
-  rows_pos: rows inner_mat > 0;
-  cols_pos: cols inner_mat > 0;
+  Mbits: nat;
+  Minner: nat -> nat -> C;
 }.
 
-Notation "m .rows" := (rows (inner_mat m)) (at level 9, format "m '.rows'").
-Notation "m .cols" := (cols (inner_mat m)) (at level 9, format "m '.cols'").
-Notation "m .inner" := (inner (inner_mat m)) (at level 9, format "m '.inner'").
+Definition Msize (m: Matrix): nat := Nat.pow 2 (Mbits m).
+
+
+(* Notation "m .msize" := (2 ^ (bits_mat m))%nat (at level 9, format "m '.msize'").
+Notation "m .minner" := (inner_mat m) (at level 9, format "m '.minner'"). *)
+
+(* ============================================================================================== *)
+(* Vector record ================================================================================ *)
+
+Record RowVec: Type := {
+  RVbits: nat;
+  RVinner: nat -> C;
+}.
+
+Record ColVec: Type := {
+  CVbits: nat;
+  CVinner: nat -> C;
+}.
+
+Definition RVsize (r: RowVec): nat := Nat.pow 2 (RVbits r).
+Definition CVsize (c: ColVec): nat := Nat.pow 2 (CVbits c).
+
+Definition MMeqbits (m1 m2: Matrix): Prop := Mbits m1 = Mbits m2.
+Definition MReqbits (m: Matrix) (r: RowVec): Prop := Mbits m = RVbits r.
+Definition RMeqbits (r: RowVec) (m: Matrix) : Prop := RVbits r = Mbits m.
+Definition MCeqbits (m: Matrix) (c: ColVec): Prop := Mbits m = CVbits c.
+Definition CMeqbits (c: ColVec) (m: Matrix): Prop := CVbits c = Mbits m.
+Definition RReqbits (r1 r2: RowVec): Prop := RVbits r1 = RVbits r2.
+Definition RCeqbits (r: RowVec) (c: ColVec): Prop := RVbits r = CVbits c.
+Definition CReqbits (c: ColVec) (r: RowVec): Prop := CVbits c = RVbits r.
+Definition CCeqbits (c1 c2: ColVec): Prop := CVbits c1 = CVbits c2.
 
 (* ============================================================================================== *)
 (* get an element from a matrix ================================================================= *)
 
-Definition Mget (m : Matrix) (i j: nat) (Hi: i < m.rows) (Hj: j < m.cols): C := m.inner i j.
+Definition Mget (m : Matrix) (i j: nat) (Hi: i < Msize m) (Hj: j < Msize m): C := Minner m i j.
 
 Notation "m '[[' i Hi '|' j Hj ']]'" :=
   (Mget m i j Hi Hj) (at level 10, i at level 9, Hi at level 9, j at level 9, Hj at level 9, no associativity).
 
 (* ============================================================================================== *)
-(* row and column vectors ======================================================================= *)
+(* get an element from a vector ================================================================= *)
 
-Definition extract_row_inner (m: Matrix_inner) (i: nat): {m': Matrix_inner | rows m' = 1 /\ cols m' = cols m}.
+Definition RVget (r : RowVec) (j: nat) (Hj: j < RVsize r): C := RVinner r j.
+Definition CVget (c : ColVec) (i: nat) (Hi: i < CVsize c): C := CVinner c i.
+
+(* ============================================================================================== *)
+(* extract row and column vectors from a matrix ================================================= *)
+
+Definition extract_row (m: Matrix) (i: nat) (Hi: i < Msize m): {r: RowVec | RMeqbits r m}.
 Proof.
-  refine ( exist _ {|
-    rows := 1;
-    cols := cols m;
-    inner := fun i' j' => inner m (i + i')%nat (j');
+  refine (exist _ {|
+    RVbits := Mbits m;
+    RVinner := fun j => Minner m i j;
     |} _ ).
-  split. reflexivity. reflexivity.
+  reflexivity.
 Defined.
 
-Definition extract_row (m: Matrix) (i: nat) (H: i < m.rows): {m': Matrix | m'.rows = 1 /\ m'.cols = m.cols}.
+Definition extract_col (m: Matrix) (j: nat) (Hj: j < Msize m): {c: ColVec | CMeqbits c m}.
 Proof.
-  destruct (extract_row_inner (inner_mat m) i) as [m' Hm'].
-  refine ( exist _
-    {|inner_mat := m' |} _).
-  Unshelve.
-  - simpl. apply Hm'.
-  - simpl. lia.
-  - destruct Hm' as [_ Hm].
-    rewrite Hm.
-    apply cols_pos.
+  refine (exist _ {|
+    CVbits := Mbits m;
+    CVinner := fun i => Minner m i j;
+    |} _ ).
+  reflexivity.
 Defined.
 
-Property extract_row_correct: forall
-  (m r: Matrix) (i j: nat) (Hi Hi': _) (Hj: _) (Hr: _) (Hir: _) (Hjr: _),
+Lemma extract_row_correct: forall
+  (m: Matrix) (r: RowVec) (i j: nat) (Hi Hi': _) (Hj: _) (Hr: _) (Hjr: _),
   exist _ r Hr = extract_row m i Hi ->
-  r[[0 Hir|j Hjr]] = m[[i Hi'|j Hj]].
+  RVget r j Hjr = m[[i Hi'|j Hj]].
 Proof.
   unfold extract_row.
   intros.
   inversion H.
+  unfold RVget.
   unfold Mget.
   rewrite H1.
-  intros.
   simpl.
-  replace (i + 0)%nat with i by lia.
   reflexivity.
 Qed.
 
-Definition extract_col_inner (m: Matrix_inner) (j: nat): {m': Matrix_inner | rows m' = rows m /\ cols m' = 1}.
-Proof.
-  refine ( exist _ {|
-    rows := rows m;
-    cols := 1;
-    inner := fun i' j' => inner m (i')%nat (j + j');
-    |} _ ).
-  split. reflexivity. reflexivity.
-Defined.
-
-Definition extract_col (m: Matrix) (j: nat) (H: j < m.cols): {m': Matrix | m'.rows = m.rows /\ m'.cols = 1}.
-Proof.
-  destruct (extract_col_inner (inner_mat m) j) as [m' Hm'].
-  refine ( exist _ {|inner_mat := m'|} _).
-  Unshelve.
-  - simpl. apply Hm'.
-  - destruct Hm' as [Hm _].
-    rewrite Hm.
-    apply rows_pos.
-  - simpl. lia.
-Defined.
-
-Property extract_col_correct: forall
-  (m c: Matrix) (i j: nat)
-  (Hi: _) (Hj Hj': _) (Hc: _) (Hic: _) (Hjc: _),
+Lemma extract_col_correct: forall
+  (m: Matrix) (c: ColVec) (i j: nat) (Hi: _) (Hj Hj': _) (Hc: _) (Hic: _),
   exist _ c Hc = extract_col m j Hj ->
-  c[[i Hic|0 Hjc]] = m[[i Hi|j Hj']].
+  CVget c i Hic = m[[i Hi|j Hj']].
 Proof.
   unfold extract_col.
   intros.
   inversion H.
+  unfold CVget.
   unfold Mget.
   rewrite H1.
-  intros.
   simpl.
-  replace (j + 0)%nat with j by lia.
   reflexivity.
 Qed.
 
-Fixpoint dot_product_inner (r c: nat -> nat -> C) (idx: nat): C.
+Fixpoint dot_product_suppl (r c: nat -> C) (idx: nat): C.
 Proof.
   destruct idx as [|idx'].
   - exact O.
-  - apply (r O idx' * c idx' O + dot_product_inner r c idx').
+  - apply (r idx' * c idx' + dot_product_suppl r c idx').
 Defined.
 
-Definition dot_product (r c: Matrix) (Hr: r.rows = 1) (Hc: c.cols = 1) (Hrc: r.cols = c.rows): C.
-Proof.
-  refine (dot_product_inner (inner (inner_mat r)) (inner (inner_mat c)) (r.cols)).
-Defined.
+Definition dot_product (r: RowVec) (c: ColVec) (Hrc: RCeqbits r c): C :=
+  dot_product_suppl (RVinner r) (CVinner c) (RVsize r).
 
-Fact eq_dot_product: forall (m1 m2 m3 m4: Matrix) (H1: _) (H2: _) (H3: _) (H4: _) (H5: _) (H6: _),
+(* Fact eq_dot_product: forall (m1 m2 m3 m4: Matrix) (H1: _) (H2: _) (H3: _) (H4: _) (H5: _) (H6: _),
   m1 = m3 -> m2 = m4 -> dot_product m1 m2 H1 H2 H3 = dot_product m3 m4 H4 H5 H6.
 Proof.
   intros.
@@ -139,31 +134,18 @@ Proof.
   rewrite HP2.
   rewrite HP3.
   reflexivity.
-Qed.
+Qed. *)
 
 (* ============================================================================================== *)
 (* element-wise unary operation ================================================================= *)
 
-Definition Muop_inner (uop: C -> C) (m: Matrix_inner): {m': Matrix_inner | rows m' = rows m /\ cols m' = cols m}.
+Definition Muop (uop: C -> C) (m: Matrix): {m': Matrix | MMeqbits m' m}.
 Proof.
   refine ( exist _ {|
-    rows := rows m;
-    cols := cols m;
-    inner := fun i j => uop (inner m i j);
+    Mbits := Mbits m;
+    Minner := fun i j => uop (Minner m i j);
     |} _ ).
-  simpl. split. reflexivity. reflexivity.
-Defined.
-
-Definition Muop (uop: C -> C) (m: Matrix): {m': Matrix | m'.rows = m.rows /\ m'.cols = m.cols}.
-Proof.
-  destruct (Muop_inner uop (inner_mat m)) as [m' Hm'].
-  refine (exist _ {|inner_mat := m'|} _).
-  simpl. apply Hm'.
-  Unshelve.
-  - destruct Hm' as [H1 H2].
-    rewrite H1. apply rows_pos.
-  - destruct Hm' as [H1 H2].
-    rewrite H2. apply cols_pos.
+  reflexivity.
 Defined.
 
 Property Muop_correct: forall
@@ -183,8 +165,7 @@ Qed.
 (* ============================================================================================== *)
 (* opposite of a matrix ========================================================================= *)
 
-Definition Mopp (m: Matrix): {m': Matrix | m'.rows = m.rows  /\ m'.cols = m.cols} :=
-  Muop Copp m.
+Definition Mopp (m: Matrix): {m': Matrix | MMeqbits m' m} := Muop Copp m.
 
 Notation "- x" := (Mopp x) : M_scope.
 
@@ -200,8 +181,7 @@ Qed.
 (* ============================================================================================== *)
 (* scalar multiplication ======================================================================== *)
 
-Definition Msmul (s: C) (m: Matrix): {m': Matrix | m'.rows = m.rows  /\ m'.cols = m.cols} :=
-  Muop (Cmult s) m.
+Definition Msmul (s: C) (m: Matrix): {m': Matrix | MMeqbits m' m} := Muop (Cmult s) m.
 
 Property Msuml_correct: forall
   (s: C) (m1 m2: Matrix) (i j: nat)
@@ -215,32 +195,19 @@ Qed.
 (* ============================================================================================== *)
 (* element-wise binary operation ================================================================ *)
 
-Definition Mbop_inner (bop: C -> C -> C) (m1 m2: Matrix_inner):
-  {m: Matrix_inner | rows m = rows m1 /\ cols m = cols m1}.
+Definition Mbop (bop: C -> C -> C) (m1 m2: Matrix) (Hbits: MMeqbits m1 m2):
+  {m: Matrix| MMeqbits m m1}.
 Proof.
   refine (exist _ {|
-    rows := rows m1;
-    cols := cols m1;
-    inner := fun i j => bop (inner m1 i j) (inner m2 i j) |} _ ).
-  simpl. split. reflexivity. reflexivity.
-Defined.
-
-Definition Mbop (bop: C -> C -> C) (m1 m2: Matrix) (Hrows: m1.rows = m2.rows) (Hcols: m1.cols = m2.cols):
-  {m: Matrix | m.rows = m1.rows /\ m.cols = m1.cols}.
-Proof.
-  destruct (Mbop_inner bop (inner_mat m1) (inner_mat m2)) as [m Hm].
-  destruct Hm as [H1 H2].
-  refine (exist _ {|inner_mat:= m|} _).
-  Unshelve.
-  - simpl. split. lia. lia.
-  - rewrite H1. apply rows_pos.
-  - rewrite H2. apply cols_pos.
+    Mbits := Mbits m1;
+    Minner := fun i j => bop (Minner m1 i j) (Minner m2 i j) |} _ ).
+  simpl. reflexivity.
 Defined.
 
 Property Mbop_correct: forall
   (bop: C -> C -> C) (m1 m2 m3: Matrix) (i j: nat)
-  (Hrows: _) (Hcols: _) (Hbop: _) (H1i: _) (H1j: _) (H2i: _) (H2j: _) (H3i: _) (H3j: _),
-  exist _ m3 Hbop = Mbop bop m1 m2 Hrows Hcols ->
+  (Hbits: _) (Hbop: _) (H1i: _) (H1j: _) (H2i: _) (H2j: _) (H3i: _) (H3j: _),
+  exist _ m3 Hbop = Mbop bop m1 m2 Hbits ->
   m3[[i H3i|j H3j]] = bop (m1[[i H1i|j H1j]]) (m2[[i H2i|j H2j]]).
 Proof.
   unfold Mbop, Mget.
@@ -253,15 +220,12 @@ Qed.
 (* ============================================================================================== *)
 (* matrix addition ============================================================================== *)
 
-Definition Mplus
-  (m1 m2: Matrix) (Hrows: m1.rows = m2.rows) (Hcols: m1.cols = m2.cols):
-  {m: Matrix | m.rows = m1.rows /\ m.cols = m1.cols} :=
-  Mbop Cplus m1 m2 Hrows Hcols.
+Definition Mplus (m1 m2: Matrix) (Hbits: MMeqbits m1 m2) := Mbop Cplus m1 m2 Hbits.
 
 Property Mplus_correct: forall
   (m1 m2 m3: Matrix) (i j: nat)
-  (Hrows: _) (Hcols: _) (Hbop: _) (H1i: _) (H1j: _) (H2i: _) (H2j: _) (H3i: _) (H3j: _),
-  exist _ m3 Hbop = Mplus m1 m2 Hrows Hcols ->
+  (Hbits: _) (Hbop: _) (H1i: _) (H1j: _) (H2i: _) (H2j: _) (H3i: _) (H3j: _),
+  exist _ m3 Hbop = Mplus m1 m2 Hbits ->
   m3[[i H3i|j H3j]] = Cplus (m1[[i H1i|j H1j]]) (m2[[i H2i|j H2j]]).
 Proof.
   apply Mbop_correct.
@@ -270,15 +234,12 @@ Qed.
 (* ============================================================================================== *)
 (* matrix subtraction =========================================================================== *)
 
-Definition Mminus
-  (m1 m2: Matrix) (Hrows: m1.rows = m2.rows) (Hcols: m1.cols = m2.cols):
-  {m: Matrix | m.rows = m1.rows /\ m.cols = m1.cols} :=
-  Mbop Cminus m1 m2 Hrows Hcols.
+Definition Mminus (m1 m2: Matrix) (Hbits: MMeqbits m1 m2) := Mbop Cminus m1 m2 Hbits.
 
 Property Mminus_correct: forall
   (m1 m2 m3: Matrix) (i j: nat)
-  (Hrows: _) (Hcols: _) (Hbop: _) (H1i: _) (H1j: _) (H2i: _) (H2j: _) (H3i: _) (H3j: _),
-  exist _ m3 Hbop = Mminus m1 m2 Hrows Hcols ->
+  (Hbits: _) (Hbop: _) (H1i: _) (H1j: _) (H2i: _) (H2j: _) (H3i: _) (H3j: _),
+  exist _ m3 Hbop = Mminus m1 m2 Hbits ->
   m3[[i H3i|j H3j]] = Cminus (m1[[i H1i|j H1j]]) (m2[[i H2i|j H2j]]).
 Proof.
   apply Mbop_correct.
@@ -287,15 +248,12 @@ Qed.
 (* ============================================================================================== *)
 (* matrix element-wise multiplication =========================================================== *)
 
-Definition Meltmul
-  (m1 m2: Matrix) (Hrows: m1.rows = m2.rows) (Hcols: m1.cols = m2.cols):
-  {m: Matrix | m.rows = m1.rows /\ m.cols = m1.cols} :=
-  Mbop Cmult m1 m2 Hrows Hcols.
+Definition Meltmul (m1 m2: Matrix) (Hbits: MMeqbits m1 m2) := Mbop Cmult m1 m2 Hbits.
 
 Property Meltmul_correct: forall
   (m1 m2 m3: Matrix) (i j: nat)
-  (Hrows: _) (Hcols: _) (Hbop: _) (H1i: _) (H1j: _) (H2i: _) (H2j: _) (H3i: _) (H3j: _),
-  exist _ m3 Hbop = Meltmul m1 m2 Hrows Hcols ->
+  (Hbits: _) (Hbop: _) (H1i: _) (H1j: _) (H2i: _) (H2j: _) (H3i: _) (H3j: _),
+  exist _ m3 Hbop = Meltmul m1 m2 Hbits ->
   m3[[i H3i|j H3j]] = Cmult (m1[[i H1i|j H1j]]) (m2[[i H2i|j H2j]]).
 Proof.
   apply Mbop_correct.
@@ -311,7 +269,7 @@ Proof.
   refine (dot_product_inner (inner r) (inner c) (cols r)).
 Defined.
 
-Definition Mmult (m1 m2: Matrix) (H: m1.cols = m2.rows):
+Definition Mmult (m1 m2: Matrix) (H: Mbits m1 = Mbits m2).
   {m: Matrix | m.rows = m1.rows /\ m.cols = m2.cols}.
 Proof.
   refine( exist _
