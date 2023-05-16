3ac348785d900b871a7f3406c2ab2475c4ff53a4
Kanguk Lee <p51lee@kaist.ac.kr>
2023-05-16 11:05:02 +0900

    Define projection operators

--- src/Operator.v
+++ src/Operator.v
@@ -40,6 +40,47 @@ Defined.
 
 Local Close Scope R_scope.
 
+(* ============================================================================================== *)
+(* single qubit projection operators ============================================================ *)
+
+Definition Qproj0: Matrix := {|
+  Mbits := 1;
+  Minner := fun i j => match i, j with
+    | 0, 0 => 1
+    | _, _ => 0
+    end;
+  |}.
+
+Definition Qproj1: Matrix := {|
+  Mbits := 1;
+  Minner := fun i j => match i, j with
+    | 1, 1 => 1
+    | _, _ => 0
+    end;
+  |}.
+
+(* ============================================================================================== *)
+(* generalization of single qubit operators ===================================================== *)
+
+Definition Qopsq (n t: nat) (op: Matrix)
+  (Ht: t < n) (Hop: Mbits op = 1): {m: Matrix | Mbits m = n}.
+Proof.
+  destruct (eye t) as [eye1 Heye1].
+  destruct (eye (n - t - 1)) as [eye2 Heye2].
+  destruct (TMproduct eye1 op) as [m' Hm'].
+  destruct (TMproduct m' eye2) as [m Hm].
+  refine (exist _ m _).
+  rewrite Hm.
+  rewrite Hm'.
+  rewrite Heye1.
+  rewrite Heye2.
+  rewrite Hop.
+  replace ((t + 1 + (n - t - 1))%nat) with (((n - t - 1) + 1 + t)%nat) by lia.
+  repeat rewrite Nat.sub_add.
+  reflexivity. lia. lia.
+Qed.
+
+(* ============================================================================================== *)
 (* swap operator ================================================================================ *)
 
 Definition Qswap2: Matrix := {|
@@ -52,6 +93,8 @@ Definition Qswap2: Matrix := {|
     | _, _ => 0
     end;
   |}.
+
+Eval compute in (Minner Qswap2 1 2).
 
 Fixpoint Qswap1n (n: nat) (H: n > 1): {m: Matrix | Mbits m = n}.
 Proof.
@@ -216,102 +259,4 @@ Defined.
 (* ============================================================================================== *)
 (* quantum qubit operator ======================================================================= *)
 
-Inductive Qoperator: nat -> Matrix -> Prop :=
-
-(* ============================================================================================== *)
-(* sequence of operators (not a multiplication) ================================================= *)
-
-Definition Qop_seq (qop1 qop2: Qoperator) (H: bits_qop qop1 = bits_qop qop2)
-  : {qop: Qoperator | bits_qop qop = bits_qop qop1}.
-Proof.
-  refine ( exist _ {|
-    bits_qop := bits_qop qop1;
-    (* NOTE: qop1 is applied first: m = m2 * m1 *)
-    inner_qop := (Mmult (inner_qop qop2) (inner_qop qop1) _).1;
-    |} _ ).
-  Unshelve.
-  - reflexivity.
-  - rewrite inner_rows_qop.
-    rewrite inner_cols_qop.
-    rewrite H.
-    reflexivity.
-  - simpl.
-    rewrite inner_rows_qop.
-    rewrite H.
-    reflexivity.
-  - simpl.
-    rewrite inner_cols_qop.
-    rewrite H.
-    reflexivity.
-Qed.
-
-(* ============================================================================================== *)
-(* fundamental qubit operators ================================================================== *)
-
-Definition Qop_id (bits: nat): {qop: Qoperator | bits_qop qop = bits}.
-Proof.
-  refine ( exist _ {|
-    bits_qop := bits;
-    inner_qop := (eye (2 ^ bits) _).1;
-    |} _ ).
-  Unshelve.
-  - reflexivity.
-  - induction bits as [|bits].
-    * simpl. lia.
-    * simpl. lia.
-  - reflexivity.
-  - reflexivity.
-Defined.
-
-
-Definition Qop_ry (theta: R): {qop: Qoperator | bits_qop qop = 1}.
-Proof.
-Local Open Scope R_scope.
-  refine ( exist _ {|
-    bits_qop := 1;
-    inner_qop := {|
-      rows := 2;
-      cols := 2;
-      inner := fun i j =>
-        if i =? 0 then if j =? 0 then cos (theta / 2) else - sin (theta / 2)
-        else if j =? 0 then           sin (theta / 2) else   cos (theta / 2);
-      |}
-    |} _ ).
-  Unshelve.
-  reflexivity. lia. lia. reflexivity. reflexivity.
-Local Close Scope R_scope.
-Qed.
-
-Definition Qop_rz (theta: R): {qop: Qoperator | bits_qop qop = 1}.
-Proof.
-Local Open Scope R_scope.
-  refine ( exist _ {|
-    bits_qop := 1;
-    inner_qop := {|
-      rows := 2;
-      cols := 2;
-      inner := fun i j =>
-        if i =? 0 then if j =? 0 then Cexp (0, - theta / 2) else          0
-        else if j =? 0 then                     0           else Cexp (0, theta / 2);
-      |}
-    |} _ ).
-  Unshelve.
-  reflexivity. lia. lia. reflexivity. reflexivity.
-Local Close Scope R_scope.
-Qed.
-
-Definition Qop_rot (theta phi lambda: R): {qop: Qoperator | bits_qop qop = 1}.
-Proof.
-  destruct (Qop_rz lambda) as [rzl Hrzl].
-  destruct (Qop_ry theta) as [ryt Hryt].
-  destruct (Qop_rz phi) as [rzp Hrzp].
-  assert (bits_qop rzl = bits_qop ryt) as H1 by lia.
-  destruct (Qop_seq rzl ryt H1) as [rzy Hzy].
-  assert (bits_qop rzy = bits_qop rzp) as H2 by lia.
-  destruct (Qop_seq rzy rzp H2) as [r Hr].
-  refine (exist _ r _).
-  lia.
-Defined.
-
-(* ============================================================================================== *)
-(* ============================================================================================== *)
+(* Inductive Qoperator: nat -> Matrix -> Prop := *)
