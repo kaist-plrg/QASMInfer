b4a22a12debdbdf831b63df049146c0633779f6b
Kanguk Lee <p51lee@kaist.ac.kr>
2023-10-31 15:00:53 +0900

    Add install.sh for install qasm

---
+++ qasm/install.sh
@@ -0,0 +1,9 @@
+# check if opam local switch already exists
+if [ ! -d "_opam" ]; then
+  opam switch -y create . --deps-only --packages=ocaml-base-compiler.4.14.0 && \
+  dune build && \
+  opam install --deps-only .
+else
+  dune build && \
+  opam install --deps-only .
+fi
