c8cd9442dcbaa9f542b142e233e3717a61d6e8f4
Kanguk Lee <p51lee@kaist.ac.kr>
2023-11-16 17:39:29 +0900

    Disable reset optimization

--- qasm/lib/desugarer/desugar.ml
+++ qasm/lib/desugarer/desugar.ml
@@ -409,7 +409,16 @@ let rec desugar_qcir_program (qc_ir_program : qc_ir) (acc : int) :
   | RotateIr (t, p, l, q) -> (RotateInstr (t, p, l, q), acc)
   | CnotIr (a1, a2) -> (CnotInstr (a1, a2), acc)
   | MeasureIr (q, c) -> (MeasureInstr (q, c), acc)
-  | ResetIr q -> (ResetInstr q, acc)
+  | ResetIr q ->
+      ( SeqInstr
+          ( MeasureInstr (q, acc),
+            IfInstr
+              ( acc,
+                true,
+                RotateInstr
+                  (float_to_R Float.pi, float_to_R 0.0, float_to_R Float.pi, q)
+              ) ),
+        acc )
   | SeqIr (ir1, ir2) ->
       let qc1, acc1 = desugar_qcir_program ir1 acc in
       let qc2, acc2 = desugar_qcir_program ir2 acc1 in
@@ -444,7 +453,7 @@ let desugar qasm =
   let qc_program : inlinedProgram =
     {
       iP_num_qbits = num_qbits;
-      iP_num_cbits = num_cbits;
+      iP_num_cbits = num_cbits + 1;
       iP_num_subinstrs = Int.max_int;
       iP_instrs = qasm_core;
     }
