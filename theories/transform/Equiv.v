8cb106035ec0e5f8d78f8080b68b89a51ed91854
Youjin Jang <jangyoujin2003@gmail.com>
2026-01-14 16:44:13 +0900

    rewrite rule proof

--- theories/transform/Equiv.v
+++ theories/transform/Equiv.v
@@ -9,6 +9,7 @@ From Stdlib.FSets Require Import FMapPositive FMapFacts.
 
 Module PFacts := WFacts_fun PositiveMap.E PositiveMap.
 Module PProperties := WProperties_fun PositiveMap.E PositiveMap.
+Module POrd := OrdProperties PositiveMap.
 
 Open Scope nat_scope.
 Bind Scope nat_scope with nat.
@@ -152,6 +153,150 @@ Proof.
 Qed.
 
 (* ============================================================================================== *)
+(* Proof about equivalance preservation (Proper) of Execute_suppl =============================== *)
+
+Lemma fold_left_eqlistA_Proper
+  {A B} (RA : relation A) (RB : relation B)
+  (step : B -> A -> B) :
+  (forall a1 a2 b1 b2, RA a1 a2 -> RB b1 b2 -> RB (step b1 a1) (step b2 a2)) ->
+  forall l1 l2 acc1 acc2,
+    eqlistA RA l1 l2 ->
+    RB acc1 acc2 ->
+    RB (fold_left step l1 acc1) (fold_left step l2 acc2).
+Proof.
+  intros Hstep l1 l2 acc1 acc2 Hel Hacc.
+  revert acc1 acc2 Hacc.
+  induction Hel; intros acc1 acc2 Hacc; simpl.
+  - exact Hacc.
+  - apply IHHel.
+    apply (Hstep x x' acc1 acc2); assumption.
+Qed.
+
+Lemma PositiveMap_fold_Proper_gen
+  (f : positive -> Branch nq -> ProgramState nq -> ProgramState nq):
+  (forall k b, Proper (ProgramState_equiv ==> ProgramState_equiv) (f k b)) ->
+  Proper (ProgramState_equiv ==> ProgramState_equiv ==> ProgramState_equiv)
+         (fun ps base_ps => PositiveMap.fold f ps base_ps).
+Proof.
+  intros Hf ps1 ps2 Heq base1 base2 Heqbase.
+  rewrite PositiveMap.fold_1, PositiveMap.fold_1.
+  apply (fold_left_eqlistA_Proper (POrd.O.eqke (elt:=Branch nq))).
+  - intros [k1 b1] [k2 b2] acc1 acc2 Hpq Hacc.
+    destruct Hpq as [Hk Hb]. simpl in *.
+    rewrite Hk, Hb.
+    apply Hf. apply Hacc.
+  - apply POrd.elements_Equal_eqlistA.
+    apply Heq.
+  - apply Heqbase.
+Qed.
+
+Lemma PositiveMap_add_Proper (k: positive) (b: Branch nq):
+  Proper (ProgramState_equiv ==> ProgramState_equiv) (PositiveMap.add k b).
+Proof.
+  intros ps1 ps2 Heq.
+  unfold ProgramState_equiv, PositiveMap.Equal in *.
+  intros k'.
+  rewrite PFacts.add_o, PFacts.add_o.
+  destruct (PProperties.F.eq_dec k k').
+  - reflexivity.
+  - apply Heq.
+Qed.
+
+Lemma Execute_rotate_instr_Proper (theta phi lambda: R) (target: nat):
+  Proper (ProgramState_equiv ==> ProgramState_equiv) (Execute_rotate_instr nq theta phi lambda target).
+Proof.
+  intros ps1 ps2 Heq cstate.
+  unfold Execute_rotate_instr.
+  rewrite PFacts.map_o, PFacts.map_o.
+  f_equal.
+  apply Heq.
+Qed.
+
+Lemma Execute_cnot_instr_Proper (control target: nat):
+  Proper (ProgramState_equiv ==> ProgramState_equiv) (Execute_cnot_instr nq control target).
+Proof.
+  intros ps1 ps2 Heq cstate.
+  unfold Execute_cnot_instr.
+  rewrite PFacts.map_o, PFacts.map_o.
+  f_equal.
+  apply Heq.
+Qed.
+
+Lemma Execute_swap_instr_Proper (q1 q2: nat):
+  Proper (ProgramState_equiv ==> ProgramState_equiv) (Execute_swap_instr nq q1 q2).
+Proof.
+  intros ps1 ps2 Heq cstate.
+  unfold Execute_swap_instr.
+  rewrite PFacts.map_o, PFacts.map_o.
+  f_equal.
+  apply Heq.
+Qed.
+
+Lemma ProgramState_merge_step_Proper (k: positive) (b: Branch nq):
+  Proper (ProgramState_equiv ==> ProgramState_equiv) (merge_step nq k b).
+Proof.
+  intros ps1 ps2 Heq.
+  unfold merge_step.
+  rewrite Heq.
+  destruct (PositiveMap.find k ps2).
+  - apply PositiveMap_add_Proper. apply Heq.
+  - apply PositiveMap_add_Proper. apply Heq.
+Qed.
+
+Lemma ProgramState_merge_Proper:
+  Proper (ProgramState_equiv ==> ProgramState_equiv ==> ProgramState_equiv) (ProgramState_merge nq).
+Proof.
+  intros ps1 ps2 Heq ps1' ps2' Heq'.
+  unfold ProgramState_merge.
+  apply PositiveMap_fold_Proper_gen; try assumption.
+  apply ProgramState_merge_step_Proper.
+Qed.
+
+Lemma Execute_measure_instr_Proper (qbit cbit: nat):
+  Proper (ProgramState_equiv ==> ProgramState_equiv) (Execute_measure_instr nq qbit cbit).
+Proof.
+  intros ps1 ps2 Heq cstate.
+  unfold Execute_measure_instr.
+  apply PositiveMap_fold_Proper_gen.
+  - intros k b ps1' ps2' Heq'.
+    apply ProgramState_merge_Proper.
+    + apply Heq'.
+    + apply ProgramState_equiv_equivalence.
+  - apply Heq.
+  - apply ProgramState_equiv_equivalence.
+Qed.
+
+Lemma Execute_reset_instr_Proper (target: nat):
+  Proper (ProgramState_equiv ==> ProgramState_equiv) (Execute_reset_instr nq target).
+Proof.
+  intros ps1 ps2 Heq cstate.
+  unfold Execute_reset_instr.
+  rewrite PFacts.map_o, PFacts.map_o.
+  f_equal.
+  apply Heq.
+Qed.
+
+Lemma Execute_suppl_Proper (instr: Instruction):
+  Proper (ProgramState_equiv ==> ProgramState_equiv) (Execute_suppl nq instr).
+Proof.
+  induction instr; intros ps1 ps2 Heq; simpl.
+  - assumption.
+  - apply Execute_rotate_instr_Proper. assumption.
+  - apply Execute_cnot_instr_Proper. assumption.
+  - apply Execute_swap_instr_Proper. assumption.
+  - apply Execute_measure_instr_Proper. assumption.
+  - apply IHinstr2. apply IHinstr1. assumption.
+  - apply PositiveMap_fold_Proper_gen.
+    + intros k b' ps1' ps2' Heq'.
+      apply ProgramState_merge_Proper.
+      * apply Heq'.
+      * apply ProgramState_equiv_equivalence.
+    + apply Heq.
+    + apply ProgramState_equiv_equivalence.
+  - apply Execute_reset_instr_Proper. assumption.
+Qed.
+
+(* ============================================================================================== *)
 (* Proof of equivalence ========================================================================= *)
 
 Lemma ProgramState_equiv_implies_behavioral_equiv:
@@ -194,6 +339,24 @@ Proof.
   simpl.
   unfold ProgramState_equiv.
   remember (Execute_suppl nq pre ps) as ps'.
-Admitted.
+  assert (Hinv': ProgramState_invariant nq ps').
+  {
+    rewrite Heqps'. apply Execute_suppl_valid_invariant. apply Hinv.
+  }
+  apply Execute_suppl_Proper.
+  apply (Hequiv ps' Hinv').
+Qed.
+
+Theorem Instruction_equiv_nop:
+  forall (pre_instr post_instr: Instruction),
+  Instruction_equiv
+  (SeqInstr pre_instr (SeqInstr NopInstr post_instr))
+  (SeqInstr pre_instr post_instr).
+Proof.
+  intros pre post ps Hinv.
+  simpl.
+  apply Execute_suppl_Proper.
+  apply ProgramState_equiv_equivalence.
+Qed.
 
 End Equivalence.
